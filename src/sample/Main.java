160a8b4410f6ad56388c2224ffd1d4affc602632
linusromland <48645689+linusromland@users.noreply.github.com>
2020-03-25 18:46:41 +0100

    stuff

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
@@ -18,34 +19,46 @@ import java.io.File;
 public class Main extends Application {
 
     private HBox intro = new HBox();
+    private HBox ress = new HBox();
     private HBox menu = new HBox();
+    private int height = 600;
+    private int width = 800;
 
 
     public void start(Stage primaryStage) throws Exception{
-       setupIntro(primaryStage);
        setupMenu(primaryStage);
+       setupRes(primaryStage);
+    }
+
+    private void setupRes(Stage primaryStage){
+
+        primaryStage.setTitle("Hangman by Offlinus");
+        primaryStage.setScene(new Scene(ress, 800, 600));
+        primaryStage.show();
+
+        ComboBox res = new ComboBox();
+        res.getItems().addAll(
+                "800x600",
+                "1024x768",
+                "1280x720"
+        );
+        Button submit = new Button("Submit");
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                setupIntro(primaryStage);
+                primaryStage.setScene(new Scene(intro, 800, 600));
+            }
+        });
+        ress.getChildren().add(res);
+        ress.getChildren().add(submit);
     }
 
     private void setupIntro(Stage primaryStage){
-        String path = "src\\sample\\video.mp4";
-        primaryStage.setScene(new Scene(intro, 275, 275));
-        intro.getChildren().add(video(path, primaryStage));
-        primaryStage.setFullScreen(true);
-        primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-        primaryStage.show();
-
+        intro.getChildren().add(video("src\\sample\\video.mp4", primaryStage));
     }
 
     private void setupMenu(Stage primaryStage){
-
-        Button disablefullscreen = new Button("no fullscreen");
-        disablefullscreen.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                primaryStage.setFullScreen(false);
-            }
-        });
-        menu.getChildren().add(disablefullscreen);
 
     }
 
@@ -55,15 +68,12 @@ public class Main extends Application {
         MediaView mediaView = new MediaView(mediaPlayer);
         mediaPlayer.setMute(true);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        mediaView.setFitHeight(screenSize.getHeight());
-        mediaView.setFitWidth(screenSize.getWidth());
+        mediaView.setFitHeight(height);
+        mediaView.setFitWidth(width);
         mediaPlayer.setAutoPlay(true);
         mediaPlayer.setOnEndOfMedia(new Runnable() {
             public void run() {
-                primaryStage.setFullScreen(true);
-                primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-                primaryStage.setScene(new Scene(menu, 275, 275));
-
+                primaryStage.setScene(new Scene(menu, width, height));
             }
         });
         return mediaView;
